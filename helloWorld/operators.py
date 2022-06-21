85d0c628305409280ab84a935304496172d60412
B-Tshehla <Boitumelotshehla@gmail.com>
2022-06-21 10:34:08 +0200

    operators in python

---
+++ helloWorld/operators.py
@@ -0,0 +1,16 @@
+num1 = 12
+num2 = 7
+
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+qoutient = num1 / num2
+remainder = num1 % num2
+exponent = num1 ** num2
+
+print ("The sum is ", sum)
+print ("The difference is ", diff)
+print ("The product is ",product)
+print ("The qoutient is ",qoutient)
+print ("The remainder is ",remainder)
+print ("The exponent is ", exponent)
